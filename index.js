c3b31ab568603395addc733d6a18db5300757a14
Erfan <erfanhanifezade@gmail.com>
2021-09-21 14:38:24 +0430

    set up complete

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
+// const express = require("express");
+// const app = express();
+// const socket = require("socket.io");
+
+// const io = socket();
+
+// app.use(express.static("public"));
+
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+// });
+
+// app.listen(3000, () => console.log("listening on *:3000"));
+
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
+const http = require("http");
+const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const cors = require("cors");
-
-const PORT = process.env.PORT || 3000;
-
-app.set("view engine", "ejs");
-// app.use(express.static("public"));
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+let users = [];
 io.on("connection", (socket) => {
   console.log(socket.id);
+  users.push(socket.id);
+
+  socket.on("chat", (data) => {
+    console.log(data);
+  });
+
+  socket.on("disconnect", () => {
+    users.pop();
+  });
+
+  socket.emit("user-count", users.length);
 });
 
-app.listen(PORT, () => console.log(`running on ${PORT} port`));
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
