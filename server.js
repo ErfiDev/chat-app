b65e3b8e5edcb15f878f56d45c55853e15d65d41
Erfan <erfanhanifezade@gmail.com>
2021-09-24 23:26:36 +0330

    socket configuring

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 const io = require("socket.io")(server);
+const { addUser, getUser } = require("utils/user");
 
 nextApp.prepare().then(() => {
   app.get("*", (req, res) => {
@@ -12,14 +13,36 @@ nextApp.prepare().then(() => {
   });
 
   io.on("connect", (socket) => {
-    socket.emit("users", `${socket.id} is connected`);
+    socket.on("join", ({ username, room }, cb) => {
+      const { error, user } = addUser(socket.id, username, room);
 
-    const chats = [];
-    socket.on("chat", (data) => {
-      chats.push(data);
+      if (error) return cb(error);
+
+      socket.emit("message", {
+        user: "admin",
+        text: `${user.username} welcome to the ${user.room} room`,
+      });
+
+      socket.broadcast
+        .to(user.room)
+        .emit("message", {
+          user: "admin",
+          text: `${user.username}, has joined!`,
+        });
+      socket.join(user.room);
     });
 
-    socket.emit("all-chats", chats);
+    socket.on("sendMessage", (message, cb) => {
+      const user = getUser(socket.id);
+
+      io.to(user.room).emit("message", { user: user.username, text: message });
+
+      cb();
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
   });
 
   server.listen(3000, (err) => {
